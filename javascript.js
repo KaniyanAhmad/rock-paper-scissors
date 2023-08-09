9ea0dc660f7e630a900878ea90b5f617d4a66f43
KaniyanAhmad <kaniyanahmad1@gmail.com>
2023-08-09 00:58:58 -0700

    Functional Game Without GUI

--- javascript.js
+++ javascript.js
@@ -1,4 +1,8 @@
 
+let computerScore = 0;
+let playerScore = 0;
+let winner;
+
 
 function getComputerChoice() {
     const choice = ['rock','paper','scissors'];
@@ -6,7 +10,61 @@ function getComputerChoice() {
     return choice[randomChoice];
 }
 
-for(i=100;i>=0;i--){
-    console.log(getComputerChoice());
+function playround(playerSelection,computerSelection) {
+    if(playerSelection == computerSelection){
+        console.log("Draw! Play Again");
+        
+    } else if(playerSelection == "rock" && computerSelection == "paper") {
+        computerScore += 1;
+        console.log(`"Computer: ${computerScore} \nPlayer: ${playerScore}"`);
+    } else if(playerSelection == "paper" && computerSelection == "rock") {
+        playerScore += 1;
+        console.log(`"Computer: ${computerScore} \nPlayer: ${playerScore}"`);
+    } else if(playerSelection == "paper" && computerSelection == "scissors") {
+        computerScore += 1;
+        console.log(`"Computer: ${computerScore} \nPlayer: ${playerScore}"`);
+    } else if(playerSelection == "scissors" && computerSelection == "paper") {
+        playerScore += 1;
+        console.log(`"Computer: ${computerScore} \nPlayer: ${playerScore}"`);
+    } else if(playerSelection == "scissors" && computerSelection == "rock") {
+        computerScore += 1;
+        console.log(`"Computer: ${computerScore} \nPlayer: ${playerScore}"`);
+    } else if(playerSelection == "rock" && computerSelection == "scissors"){
+        playerScore += 1;
+        console.log(`"Computer: ${computerScore} \nPlayer: ${playerScore}"`);
+    }
+
+    
+    if(computerScore > playerScore){
+        winner = 1;
+    }else if(playerScore > computerScore){ 
+        winner = 2;
+    } else{
+        winner = 0;
+    }
+
+    return winner;
 }
 
+function game() {
+    const playerSelection = prompt("Enter Your Choice: \nRock, Paper, or Scrissors").toLowerCase();
+    const computerSelection = getComputerChoice();
+    
+    
+        winner = playround(playerSelection,computerSelection);
+        
+
+    
+}
+
+   for(i=0;i<=5;i++){
+        game();
+   }
+   
+    if(winner == 1){
+        console.log("Computer Won!");
+    }else if(winner == 2){ 
+        console.log("You Won!");
+    } else if(winner == 0){
+        console.log("Tie");
+    }
