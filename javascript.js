16cd9fee53bb2667e5b4bbe47f75e72f1dc91438
KaniyanAhmad <kaniyanahmad1@gmail.com>
2023-08-13 03:19:37 -0700

    Added New Pages

--- javascript.js
+++ javascript.js
@@ -1,8 +1,14 @@
 
+//index.html
+
+
+
+//-----------------------------------------------------------------------------------
+
+
+//game.html
 let computerScore = 0;
 let playerScore = 0;
-let winner;
-
 
 function getComputerChoice() {
     const choice = ['rock','paper','scissors'];
@@ -32,39 +38,14 @@ function playround(playerSelection,computerSelection) {
     } else if(playerSelection == "rock" && computerSelection == "scissors"){
         playerScore += 1;
         console.log(`"Computer: ${computerScore} \nPlayer: ${playerScore}"`);
-    }
-
-    
-    if(computerScore > playerScore){
-        winner = 1;
-    }else if(playerScore > computerScore){ 
-        winner = 2;
-    } else{
-        winner = 0;
-    }
-
-    return winner;
+    }   
 }
 
-function game() {
-    const playerSelection = prompt("Enter Your Choice: \nRock, Paper, or Scrissors").toLowerCase();
-    const computerSelection = getComputerChoice();
-    
-    
-        winner = playround(playerSelection,computerSelection);
-        
+//-----------------------------------------------------------------------------------
 
-    
-}
 
-   for(i=0;i<=5;i++){
-        game();
-   }
-   
-    if(winner == 1){
-        console.log("Computer Won!");
-    }else if(winner == 2){ 
-        console.log("You Won!");
-    } else if(winner == 0){
-        console.log("Tie");
-    }
+//theme.html
+
+
+
+//-----------------------------------------------------------------------------------
